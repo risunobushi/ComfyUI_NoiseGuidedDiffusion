b898c51e98446d5e44f1076a37f55adbee6da568
risunobushi <113669885+risunobushi@users.noreply.github.com>
2024-12-10 12:49:39 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,16 @@
-from .noise_guided_diffusion import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
+
+from noise_guided_diffusion import NoiseGuidedDiffusion
+
+NODE_CLASS_MAPPINGS = {
+    "NoiseGuidedDiffusion": NoiseGuidedDiffusion,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "NoiseGuidedDiffusion": "Noise Guided Diffusion",
+}
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
